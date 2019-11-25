eae3f84a053c4ebe4c595954037dde062fedba9d
albertumus <albcamina@gmail.com>
2019-11-25 22:01:21 +0100

    Practica Nueva

--- pl11/Debug/Box.cpp.o.d
+++
@@ -1,5 +0,0 @@
-Debug/Box.cpp.o: Box.cpp Box.hpp Pasajero.hpp
-
-Box.hpp:
-
-Pasajero.hpp:
--- pl11/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <Airport.hpp>
-
-int main(int argc, char **argv)
-{
-	Airport* a1 = new Airport();
-	a1->gestionarAeropuerto();
-	
-}
